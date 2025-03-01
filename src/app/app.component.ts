4fcd0a378a4119d65def1e3d1c7cbc539b91bd9d
Diego Tonguino <dtonguino@gestor.co>
2025-02-28 22:42:56 -0500

    [Incremento][Diseño Responsivo: ]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, inject, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
@@ -10,8 +11,19 @@ import { filter } from 'rxjs';
 })
 export class AppComponent {
 	private router = inject(Router);
+	private breakpointObserver = inject(BreakpointObserver);
 	isMobileMenuOpen = signal(false);
+	isMobile = signal(false);
 	currentRoute = signal('');
+
+	constructor() {
+		this.breakpointObserver.observe([Breakpoints.Handset]).subscribe((result) => {
+			this.isMobile.set(result.matches);
+			if (!result.matches) {
+				this.isMobileMenuOpen.set(false);
+			}
+		});
+	}
 
 	ngOnInit() {
 		this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
